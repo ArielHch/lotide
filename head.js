51f48c5355c0af90e85a948f9937d93889944aac
Ariel H <chinghan99@gmail.com>
2021-10-29 02:48:03 -0400

    head

---
+++ head.js
@@ -0,0 +1,19 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2B55 \u2B55 \u2B55 Assertion Passed:${actual} === ${expected} \u2B55 \u2B55 \u2B55`);
+  } else {
+    console.log(`\u274C \u274C \u274C Assertion Failed:${actual} !== ${expected} \u274C \u274C \u274C`);
+  }
+
+};
+// TEST CODE
+assertEqual("Coding is FUN", "Coding is FUN");
+assertEqual(3.5,1.5);
+
+Function head(arr) {
+  return arr[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
