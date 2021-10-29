6d773ad3a79a91a744db34403ca42ccbe519be24
Ariel H <chinghan99@gmail.com>
2021-10-29 18:02:21 -0400

    Done

--- tail.js
+++ tail.js
@@ -2,12 +2,9 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-
     console.log(`\u2B55 \u2B55 \u2B55 Assertion Passed:${actual} === ${expected} \u2B55 \u2B55 \u2B55`);
-
   } else {
     console.log(`\u274C \u274C \u274C Assertion Failed:${actual} !== ${expected} \u274C \u274C \u274C`);
-
   }
 };
 // TEST CODE
@@ -15,12 +12,12 @@ const assertEqual = function(actual, expected) {
 //assertEqual(3.5,1.5);
 
 const tail = function(arr) {
-  return (arr);
+  const newArr = arr.slice(1);
+  return newArr.length;
 };
 
-
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs", "So difficult"];
+const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
